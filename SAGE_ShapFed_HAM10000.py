ff334f8d6d49bf2f4ed475649ae6fa63a89de5dc
edabaser <edanur.baser@ozu.edu.tr>
2026-04-13 15:01:47 +0300

    Update SAGE_ShapFed_HAM10000.py

--- SAGE_ShapFed_HAM10000.py
+++ SAGE_ShapFed_HAM10000.py
@@ -403,7 +403,8 @@ class Local(object):
         self.criterion = torch.nn.CrossEntropyLoss().cuda(args.gpu_id)
         self.optimizer = torch.optim.SGD(
             self.local_model.parameters(),
-            lr=args.lr_local_training, momentum=0.9, weight_decay=1e-4
+            lr=args.lr_local_training, momentum=0.9, weight_decay=1e-4,
+            nesterov=True
         )
 
     def fixmatch_train(self, args, data_client_labeled, data_client_unlabeled, global_params, r):
